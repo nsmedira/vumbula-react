257c2017db9519c1a59f63ccaf05d813ac38c1f8
Nick Smedira <nsmedira@me.com>
2020-08-11 19:20:38 -0400

    finish chapter 3 project

--- chapter 3/starter-code/src/index.js
+++ chapter 3/starter-code/src/index.js
@@ -12,6 +12,18 @@ class Application extends React.Component {
     }
   }
 
+  onSubmit = event => {
+    event.preventDefault()
+    const name = this.name.value
+    const age = this.age.value
+    const info = {name: name, age: age}
+    const data = [...this.state.data, info]
+    // data.push(info)
+    this.setState({
+      data: data
+    })
+  }
+
   render() {
     return (
         <div className="container">
@@ -20,17 +32,19 @@ class Application extends React.Component {
           <hr/>
 
           <div className="row">
-            <form className="form-inline">
+            <form className="form-inline" onSubmit={this.onSubmit}>
               <input
                   type="text"
                   className="form-control mb-2 mr-sm-2 mb-sm-0"
                   placeholder="Name"
+                  ref={input => this.name = input}
                   />
               <div className="input-group mb-2 mr-sm-2 mb-sm-0">
                 <input
                     type="text"
                     className="form-control"
                     placeholder="Age"
+                    ref={input => this.age = input}
                     />
               </div>
               <button type="submit" className="btn btn-primary">Save</button>
@@ -40,7 +54,7 @@ class Application extends React.Component {
 
           <div className="row">
             {
-              this.state.data.map(info => <Card info={info}/>)
+              this.state.data.map((info, index) => <Card key={index} info={info}/>)
             }
           </div>
         </div>
